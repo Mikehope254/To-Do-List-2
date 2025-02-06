1d36f67b5d1c9c5821ac99c6c9ba840e02e3feed
Mikehope254 <mikehope1738@gmail.com>
2025-02-06 11:33:50 +0300

    post route created

---
+++ Backend/models/list.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    task: {
+      type: String,
+      required: true, //Task name is mandatory
+      trim: true, //removes extra space
+    },
+    completed: {
+      type: Boolean,
+      default: false, // Task is incomplete by default
+    },
+  },
+  {
+    timestamps: true, //createdAt, updatedAt fields added automatically
+  }
+);
+
+export const Todo = mongoose.model("Todo", todoSchema);
--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,39 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./db.js";
+import { connectDB } from "./config/db.js";
+import { Todo } from "./models/list.model.js";
 
-dotenv.config(); //Loads environment variables from .env
+dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI;
-console.log("MONGO URI:", process.env.TEST_VAR);
-connectDB();
+console.log(process.env.MONGO_URI); //calling .env file
+
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+//route to create a new todo(task)
+app.post("/create", async (req, res) => {
+  //create task
+  try {
+    const todo = req.body; //user data
+    //simulating missing task error
+    if (!todo) {
+      return res
+        .status(400)
+        .json({ success: false, message: "task is required" });
+    }
+    const newTodo = new Todo(todo);
+
+    await newTodo.save();
+    res
+      .status(201)
+      .json({ success: true, data: newTodo, message: "Task created" });
+  } catch (error) {
+    console.log("Error in creating product", error.message);
+    res.status(500).json({ message: "Server Error", success: false });
+  }
+});
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server is running on port 5000");
 });
